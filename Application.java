cb69a783354d35400e9a736672077187219e77ec
MatheuRezende <129076633+MatheuRezende@users.noreply.github.com>
2025-04-07 21:38:55 -0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,7 @@
+package backend;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gerenciamento de Restaurante iniciado!");
+    }
+}
